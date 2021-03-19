e3a087f24a7dd779116d1d1a786d8569e7b0fb5a
JanDirkKoersma <75176681+JanDirkKoersma@users.noreply.github.com>
2021-03-19 11:26:31 +0100

    Add files via upload

---
+++ Javascript_006/script.js
@@ -0,0 +1,34 @@
+const { makeRe } = require("minimatch");
+
+const makePizza = function () {
+  console.log("Add salt to the Flour");
+  console.log("Throw in the water and sugar mixture");
+  console.log("Kneading the dough");
+  console.log("Cover with a towel");
+  console.log("Bring this dough back to life");
+  console.log("Knead it for a minute or two");
+  console.log("Using fresh mozzarella");
+  console.log("Stretch the dough with your hands");
+  console.log("Use a ball of mozzarella");
+  console.log("Grate some fresh parmesan on top of it");
+};
+
+makePizza();
+makePizza();
+
+
+const makeSushi = function () {
+  console.log("Prep the Rice & Nori");
+  console.log("Spread the Rice on the Nori");
+  console.log("Add Fillings to the Bottom Quarter of the Nori");
+  console.log("Roll the Sushi Part Way");
+  console.log("Roll the Sushi the Rest of the Way");
+  console.log("Move to Cutting Board");
+  console.log("Cut the Sushi");
+  console.log("Serve");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+
